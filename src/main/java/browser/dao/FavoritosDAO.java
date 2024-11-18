f024ed07d7c57986616a8e0e6ffeda9c190b4617
FrankSkep <f.cornejo.pacheco@gmail.com>
2024-11-18 15:44:25 -0800

    improvements FavoritosDAO

--- src/main/java/browser/dao/FavoritosDAO.java
+++ src/main/java/browser/dao/FavoritosDAO.java
@@ -3,6 +3,7 @@ package browser.dao;
 import browser.database.Db_Connection;
 import browser.model.Favorito;
 
+import javax.swing.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -24,50 +25,49 @@ public class FavoritosDAO {
     }
 
     public void guardar(Favorito favorito) {
+        String sql = "INSERT INTO favoritos (nombre, url) VALUES (?, ?)";
+        try (Connection conn = Db_Connection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);) {
 
-        try (Connection conn = Db_Connection.getConnection()) {
-            String sql = "INSERT INTO favoritos (nombre, url) VALUES (?, ?)";
-            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-                pstmt.setString(1, favorito.getNombre());
-                pstmt.setString(2, favorito.getUrl());
-                pstmt.executeUpdate();
-            }
+            pstmt.setString(1, favorito.getNombre());
+            pstmt.setString(2, favorito.getUrl());
+            pstmt.executeUpdate();
         } catch (
                 SQLException e) {
-            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "Ocurrio un error:" + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
     public void eliminar(String nombre) {
+        String sql = "DELETE FROM favoritos WHERE nombre = ?";
+        try (Connection conn = Db_Connection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);) {
 
-        try (Connection conn = Db_Connection.getConnection()) {
-            String sql = "DELETE FROM favoritos WHERE nombre = ?";
-            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-                pstmt.setString(1, nombre);
-                pstmt.executeUpdate();
-            }
+            pstmt.setString(1, nombre);
+            pstmt.executeUpdate();
         } catch (
                 SQLException e) {
-            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "Ocurrio un error:" + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
     public void eliminarTodos() {
-        try (Connection conn = Db_Connection.getConnection()) {
-            String sql = "DELETE FROM favoritos";
-            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-                pstmt.executeUpdate();
-            }
+        String sql = "DELETE FROM favoritos";
+        try (Connection conn = Db_Connection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.executeUpdate();
         } catch (
                 SQLException e) {
-            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "Ocurrio un error:" + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
     public HashMap<String, String> obtenerTodos() {
         String sql = "SELECT nombre, url FROM favoritos";
 
-        try (Connection conn = Db_Connection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        try (Connection conn = Db_Connection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
             ResultSet rs = pstmt.executeQuery();
 
             HashMap<String, String> favoritos = new HashMap<>();
@@ -77,7 +77,7 @@ public class FavoritosDAO {
             return favoritos;
         } catch (
                 SQLException e) {
-            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "Ocurrio un error:" + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
             return new HashMap<>();
         }
     }
