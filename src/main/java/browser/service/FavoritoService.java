185d89bf71da228a6311de14c90882c2cee664a4
FrankSkep <f.cornejo.pacheco@gmail.com>
2024-11-17 21:44:35 -0800

    rename methods

--- src/main/java/browser/service/FavoritoService.java
+++ src/main/java/browser/service/FavoritoService.java
@@ -10,22 +10,22 @@ public class FavoritoService {
 
     public FavoritoService() {
         favoritos = new HashMap<>();
-        favoritos.putAll(FavoritosDAO.getInstance().obtenerFavoritos());
+        favoritos.putAll(FavoritosDAO.getInstance().obtenerTodos());
     }
 
     public void insertarFavorito(String nombre, String url) {
         favoritos.put(nombre, url);
-        FavoritosDAO.getInstance().insertarFavorito(nombre, url);
+        FavoritosDAO.getInstance().guardar(nombre, url);
     }
 
     public void eliminarFavorito(String nombre) {
         favoritos.remove(nombre);
-        FavoritosDAO.getInstance().eliminarFavorito(nombre);
+        FavoritosDAO.getInstance().eliminar(nombre);
     }
 
     public void eliminarFavoritos() {
         favoritos.clear();
-        FavoritosDAO.getInstance().eliminarFavoritos();
+        FavoritosDAO.getInstance().eliminarTodos();
     }
 
     public boolean existeFavorito(String url) {
